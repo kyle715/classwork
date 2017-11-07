1c6785a835e7c96fe4db89781885b090e69298f4
Kyle Mayers <kyle7150@gmail.com>
2017-11-07 00:42:07 +0000

    third and fourth step

--- codeandwatch/watchandcode.js
+++ codeandwatch/watchandcode.js
@@ -1,4 +1,8 @@
-["item 1", "item 2", "item 3"]
-["item 1", "item 2", "item 3"]
-["item 1", "item 2", "item 3"]
- var todos = ["item 1", "item 2", "item 3"]
+ var todos = ['item 1', 'item 2', 'item 3']
+ console.log("My Todos:", todos)
+ todos.push('item 4')
+ todos.push('item 5')
+ todos[0]
+ todos[1]
+ todos[2]
+ todos[0] = "item 1 updated"

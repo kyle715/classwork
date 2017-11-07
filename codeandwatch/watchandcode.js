46204b0d859b590534e1c65a719c0d7b776b5044
Kyle Mayers <kyle7150@gmail.com>
2017-11-07 00:33:04 +0000

    secondstep

--- codeandwatch/watchandcode.js
+++ codeandwatch/watchandcode.js
@@ -1,4 +1,3 @@
-["item 1", "item 2", "item 3"]
-["item 1", "item 2", "item 3"]
-["item 1", "item 2", "item 3"]
- var todos = ["item 1", "item 2", "item 3"]
+ var todos = ['item 1', 'item 2', 'item 3']
+ console.log("My Todos:", todos)
+ 

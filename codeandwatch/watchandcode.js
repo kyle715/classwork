ec20e60cf329dfd7c023e4dc9fd0430b1c7a2e8f
Kyle Mayers <kyle7150@gmail.com>
2017-11-09 17:17:24 +0000

    rooms available classwork

--- codeandwatch/watchandcode.js
+++ codeandwatch/watchandcode.js
@@ -1,4 +1,21 @@
-["item 1", "item 2", "item 3"]
-["item 1", "item 2", "item 3"]
-["item 1", "item 2", "item 3"]
- var todos = ["item 1", "item 2", "item 3"]
+ var Todos = ['item 1', 'item 2', 'item 3']
+ console.log("My Todos:", Todos)
+
+function addtodo(todo) {
+ Todos.push(todo);
+ console.log(Todos);
+}
+addtodo('hello there')
+
+function changeTodo(position, newValue){
+ Todos[position] = 'newValue';
+ console.log(Todos);
+}
+changeTodo(0, 'changed')
+
+function deleteTodo(position){
+ Todos.splice(position, 1)
+ console.log(Todos)
+}
+deleteTodo(0)
+

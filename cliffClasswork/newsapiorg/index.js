6209021cfcb78e5e529afe34490395dc6b425fe4
Kyle Mayers <kyle7150@gmail.com>
2017-12-02 00:42:13 +0000

    couldnt get function to work on a tag, but progress and change still made to page

--- cliffClasswork/newsapiorg/index.js
+++ cliffClasswork/newsapiorg/index.js
@@ -8,42 +8,41 @@
         success: function(response){
             if (response.status === "ok") {
              for (var i = 0; i < response.sources.length; i++){
-                var source = document.createElement("OPTION");
+                var source = document.createElement("a");
                 source.innerHTML = response.sources[i].name;
-                document.getElementById("selection").appendChild(source);
+                document.getElementById("dropdown").appendChild(source);
                 source.setAttribute('value',  response.sources[i].id);
-             }
-            }
-                    function displayNews(source){
-                        $.ajax({
-                            method: "GET",
-                            url:"https://newsapi.org/v2/top-headlines?sources=" + source,
-                            data:{apiKey:'3627f0612c5748d1842061db5487dace'},
-                            success: function(response){
-                            	document.getElementById('comeOn').innerHTML = '';
-                                for(var i = 0; i < response.articles.length; i++){
-                                    var pleaseWork = document.createElement("UL");
-                                    var itWorked =  document.createElement("LI");
-                                    var list = document.getElementById("comeOn");
-                                    var link = document.createElement("a");
-                                    pleaseWork.innerHTML = response.articles[i].title;
-                                    itWorked.innerHTML = response.articles[i].description + "<img src='" + response.articles[i].urlToImage + "'></span>";
-                                    itWorked.setAttribute('data-target', '#comeOn');
-                                    list.appendChild(link);
-                                    link.appendChild(pleaseWork);
-                                    list.appendChild(itWorked);
-                                    pleaseWork.setAttribute('id', i);
-                                    link.setAttribute('href', response.articles[i].url);
-                                }
-                            }
-                        });
-             }
-                 $('#source').submit(function(e){
-        event.preventDefault();
-        displayNews(document.getElementById("selection").value);
-    });
+                source.setAttribute('onclick',`displayNews();`);
+                source.setAttribute('id', i);
+             }}
         }
         
         
     });
 });
+	function displayNews(response){
+		var id = this.value;
+		$.ajax({
+			method:"GET",
+			url:"https://newsapi.org/v2/top-headlines?sources=" + 'bloomberg',
+            data:{apiKey:'3627f0612c5748d1842061db5487dace'},
+            success: function(response){
+            	document.getElementById('comeOn').innerHTML = '';
+            	document.getElementById('header').innerHTML = 'Top ' + 'Bloomberg ' + 'Headlines';
+            	 for(var i = 0; i < response.articles.length; i++){
+                    var pleaseWork = document.createElement("UL");
+                    var itWorked =  document.createElement("LI");
+                    var list = document.getElementById("comeOn");
+                    var link = document.createElement("a");
+                    pleaseWork.innerHTML = response.articles[i].title;
+                    itWorked.innerHTML = "<div><img src='" + response.articles[i].urlToImage + "'></span></div>" + '<br />' + response.articles[i].description;
+                    itWorked.setAttribute('data-target', '#comeOn');
+                    list.appendChild(link);
+                    link.appendChild(pleaseWork);
+                    list.appendChild(itWorked);
+                    pleaseWork.setAttribute('id', i);
+                    link.setAttribute('href', response.articles[i].url);
+                }
+            }
+		});}
+		

d73e87afe166b7d54fc71b713a994571df62cd17
Kyle Mayers <kyle7150@gmail.com>
2017-12-04 02:03:09 +0000

    cliffcheckthis

--- classQuoteGen/index.js
+++ classQuoteGen/index.js
@@ -1,63 +1,20 @@
 /*global $*/
-$(document).ready(function() {
-	var quote;
-	var author;
-  var GOTQuote;
-  var GOTChatacter;
-	function getNewQuote() {
-		$.ajax({
-			url: 'https://talaikis.com/api/quotes/random/',
-			success: function(response) {
-				quote = response.quote;
-				author = response.author;
-				$('#quote').text(quote);
-				if (author) {
-					$('#author').text('-- ' + author);
-				} else {
-					$('#author').text('- uknown');
-				}
-			}
-		});
-	}
-	function getGOTQuote() {
-		$.ajax({
-			url: 'https://got-quotes.herokuapp.com/quotes',
-			success: function(response) {
-			  console.log(response.GOTQuoteText);
-				GOTQuote = response.quote;
-		  	GOTChatacter = response.character;
-				$('#quote').text(GOTQuote);
-				$('#author').text('-- ' + GOTChatacter);
-			}
-		});
-	}
-	getNewQuote();
-	$('#get-quote').on('click', function() {
-		getNewQuote();
-		changeImageBack();
-	});
-	$('#getGOTQuote').on('click', function() {
-	  getGOTQuote();
-	  changeImage();
-	});
-	$('.fa-twitter').on('click', function() {
-	  var text = document.getElementById("quote").innerHTML;
-    var person = document.getElementById("author").innerHTML;
-		window.open('https://twitter.com/share?text=' + encodeURIComponent(text) + ' -- ' + (person));
-	});
-});
-
-$(document).ready(function() {
- $('body').addClass('class1');
-  });
- function changeImage() {
-    $('body').css('background-image', 'url(https://fsmedia.imgix.net/9d/2c/c8/ed/b229/48a4/8afc/d997d7fc94aa/daenerys-targaryen-tyrion-lannister-and-dragonsrhaegal-and-viserion-game-of-thrones.png)');
-   $('body').removeClass('class1');
-   $('body').addClass('class2')}
- function changeImageBack() {
-    $('body').css('background-image', 'url(https://i.pinimg.com/originals/83/28/15/8328156f64be106b14493fe117023646.gif)');
-    $('body').removeClass('class2');
-    $('body').addClass('class1'); }
+    $(document).ready(function () {
+        function getQuote() {
+            $.ajax({
+                method: "GET",
+                url: 'https://got-quotes.herokuapp.com/quotes',
+                success: function(response){
+                    $('#quote').text(response.quote);
+                    $('#character').text('-- ' + response.character);
+                }
+            });
+        }
+        getQuote();
+        $('#getQuote').on('click', function(){
+            getQuote();
+        });
+    });
     
     (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
@@ -66,14 +23,3 @@ $(document).ready(function() {
     js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
- 
-
-$('#get-quote').on('click', function() {
-	var element = document.getElementById("quote");
-	element.classList.remove('animate');
-	element.offsetWidth = element.offsetWidth;
-	void element.offsetWidth;
-	element.classList.add('animate');
-	false;
-	});
-   

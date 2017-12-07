56a5debb3ebab7b2f2a4d98bd86590612a3b7c13
Kyle Mayers <kyle7150@gmail.com>
2017-12-07 23:36:23 +0000

    working f to c function

--- weatherAPI/script.js
+++ weatherAPI/script.js
@@ -8,15 +8,33 @@ $(document).ready(function() {
         navigator.geolocation.getCurrentPosition(function(position){
             var latitude = position.coords.latitude;
             var longitude = position.coords.longitude;
+            var temp;
             $.ajax({
                 method: "GET",
                 url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&APPID=3cb920f810af921ff25538043ed1a70b`,
                 success: function(response) {
-                    $('#temp').text(response.main.temp);
+                    temp = response.main.temp;
+                    $('#temp').text(temp.toFixed(0));
                     $('#description').text(response.weather[0].main);
                     $('#city').text(response.name);
+                    document.getElementById('icon').innerHTML = '<img src="http://openweathermap.org/img/w/' + response.weather[0].icon + '.png">';
                 }
             });
         });
     }
 });
+function changeTemp() {
+    var temp = document.getElementById('temp').innerHTML;
+    var fc = document.getElementById('FC').innerHTML;
+    var far;
+    var cel;
+    if (fc == "C"){
+      document.getElementById('FC').innerHTML = "F";
+      far = temp * 1.8 + 32;
+      document.getElementById('temp').innerHTML = far.toFixed(0);
+    } else {
+      document.getElementById('FC').innerHTML = "C";
+      cel = (temp - 32) * .5556;
+      document.getElementById('temp').innerHTML = cel.toFixed(1);
+    }
+}

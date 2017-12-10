40ec818f46132b08d40af41b87d3c4d6faed718e
Kyle Mayers <kyle7150@gmail.com>
2017-12-10 12:17:10 +0000

    wikipedia page!!

---
+++ wikipediaPage/script.js
@@ -0,0 +1,49 @@
+/* global $
+ */
+$(document).ready(function() {
+	$("#searchTerm").keypress(function(e) {
+		if (e.keyCode === 13) {
+			var searchTerm = $("#searchTerm").val();
+			var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+			$.ajax({
+				url: url,
+				type: 'GET',
+				dataType: "json",
+				success: function(data) {
+				    document.getElementById('output').innerHTML = '';
+					document.getElementById('searchTerm').value = '';
+					for (var i = 0; i < data[1].length; i++) {
+						$("#output").prepend("<div><div class='well'><a href=" + data[3][i] + "><h2>" + data[1][i] + "</h2></a>" + "<p>" + data[2][i] + "</p></div></div>");
+					}
+				}
+			});
+		}
+	});
+	$('#search').on('click', function() {
+		var searchTerm = $('#searchTerm').val();
+		var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm + '&formart=json&callback=?';
+		$.ajax({
+			url: url,
+			type: "GET",
+			dataType: 'json',
+			success: function(data) {
+				document.getElementById('output').innerHTML = '';
+				document.getElementById('searchTerm').value = '';
+				for (var i = 0; i < data[1].length; i++) {
+					$('#output').prepend("<div><div class='well'><a href=" + data[3][i] + "><h2>" + data[1][i] + "</h2></a>" + "<p>" + data[2][i] + "</p></div></div>");
+				}
+			},
+		});
+	});
+});
+$(document).ready(function() {
+  $('input').focus(function() {
+    $('.bar1').removeClass('hide');
+    $('.bar2').removeClass('magnify');
+
+  });
+  $('input').blur(function() {
+    $('.bar1').addClass('hide');
+    $('.bar2').addClass('magnify');
+  });
+});

09df6d51c8b5dd686d3f7de184c02eeae3372e1c
Kyle Mayers <kyle7150@gmail.com>
2017-12-11 23:43:10 +0000

    normal functions work!

--- calculator/script.js
+++ calculator/script.js
@@ -1,64 +1,88 @@
 /* global $ */
- var sum = [{
-     sumVal: [0,] 
- }];
+ var sum = [];
 
  $('#btn1').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 1;
+    document.getElementById('table').value = memory + 1;
  });
  $('#btn2').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 2;
+    document.getElementById('table').value = memory + 2;
  });
  $('#btn3').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 3;
+    document.getElementById('table').value = memory + 3;
  });
  $('#btn4').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 4;
+    document.getElementById('table').value = memory + 4;
  });
  $('#btn5').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 5;
+    document.getElementById('table').value = memory + 5;
  });
  $('#btn6').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 6;
+    document.getElementById('table').value = memory + 6;
  });
  $('#btn7').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 7;
+    document.getElementById('table').value = memory + 7;
  });$('#btn8').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 8;
+    document.getElementById('table').value =memory + 8;
  });$('#btn9').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 9;
+    document.getElementById('table').value = memory + 9;
  });
  $('#btn0').on('click', function() {
     var memory = document.getElementById('table').value;
-    sum[0].sumVal.push(memory);
-    document.getElementById('table').value = 0;
+    document.getElementById('table').value = memory + 0;
  });
- $('#btn.').on('click', function() {
-    console.log('hello');
+ $('#btnDec').on('click', function() {
     var memory = document.getElementById('table').value;
     document.getElementById('table').value = memory + '.';
  });
- $('btn+').on('click',function() {
-    var b = sum[0].sumVal.split(',').map(function(item) {
-    return parseInt(item, 10);
-    });
-        console.log(b);
+ $('#btnPlus').on('click',function() {
+     var memory = document.getElementById('table').value;
+     sum.push(memory);
+     sum.push('+');
+     document.getElementById('table').value = '';
  });
+ $('#btnSub').on('click',function() {
+     var memory = document.getElementById('table').value;
+     sum.push(memory);
+     sum.push('-');
+     document.getElementById('table').value = '';
+ });
+  $('#btnMul').on('click',function() {
+     var memory = document.getElementById('table').value;
+     sum.push(memory);
+     sum.push('*');
+     document.getElementById('table').value = '';
+ });
+  $('#btnDiv').on('click',function() {
+     var memory = document.getElementById('table').value;
+     sum.push(memory);
+     sum.push('/');
+     document.getElementById('table').value = '';
+ });
+  $('#btnC').on('click',function() {
+     document.getElementById('table').value = '';
+ });
+  $('#btnCE').on('click',function() {
+      sum = [];
+      document.getElementById('table').value = '';
+ });
+ $('#btnEqual').on('click', function() {
+     var memory = document.getElementById('table').value;
+     sum.push(memory);
+     document.getElementById('table').value = '';
+     sToV();
+ });
+ function sToV(){
+     var join = sum.join('');
+     var equation = eval(join);
+     document.getElementById('table').value = equation;
+     sum = [];
+ }

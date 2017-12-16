308f4f1a0f64c90acbd2ca87215e1a8da2d3e946
Kyle Mayers <kyle7150@gmail.com>
2017-12-16 23:00:43 +0000

    more algorithm workouts

--- FCC/todaysWorkOut.js
+++ FCC/todaysWorkOut.js
@@ -1,10 +1,33 @@
-function sumAll(arr) {
-  var out = 0;
-  for (var i = Math.min(...arr); i <= Math.max(...arr); i++) {
-    out += i;
+function sumPrimes(num) {
+  var res = 0;
+
+  // Function to get the primes up to max in an array
+  function getPrimes(max) {
+    var sieve = [];
+    var i;
+    var j;
+    var primes = [];
+    for (i = 2; i <= max; ++i) {
+      if (!sieve[i]) {
+        // i has not been marked -- it is prime
+        primes.push(i);
+        for (j = i << 1; j <= max; j += i) {
+          sieve[j] = true;
+        }
+      }
+    }
+
+    return primes;
   }
-  return out;
+
+  // Add the primes
+  var primes = getPrimes(num);
+  for (var p = 0; p < primes.length; p++) {
+    res += primes[p];
+  }
+
+  return res;
 }
 
-sumAll([1, 4]);
-
+// test here
+sumPrimes(10);

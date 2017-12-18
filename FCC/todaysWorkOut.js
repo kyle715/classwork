0825b7518db797d3f0361c742a7d5cfa0480a691
Kyle Mayers <kyle7150@gmail.com>
2017-12-18 01:48:18 +0000

    more algorithms

--- FCC/todaysWorkOut.js
+++ FCC/todaysWorkOut.js
@@ -1,33 +1,21 @@
-function sumPrimes(num) {
-  var res = 0;
+function chunkArrayInGroups(arr, size) {
 
-  // Function to get the primes up to max in an array
-  function getPrimes(max) {
-    var sieve = [];
-    var i;
-    var j;
-    var primes = [];
-    for (i = 2; i <= max; ++i) {
-      if (!sieve[i]) {
-        // i has not been marked -- it is prime
-        primes.push(i);
-        for (j = i << 1; j <= max; j += i) {
-          sieve[j] = true;
-        }
-      }
+  var temp = [];
+  var result = [];
+
+  for (var a = 0; a < arr.length; a++) {
+    if (a % size !== size - 1)
+      temp.push(arr[a]);
+    else {
+      temp.push(arr[a]);
+      result.push(temp);
+      temp = [];
     }
-
-    return primes;
   }
 
-  // Add the primes
-  var primes = getPrimes(num);
-  for (var p = 0; p < primes.length; p++) {
-    res += primes[p];
-  }
-
-  return res;
+  if (temp.length !== 0)
+    result.push(temp);
+  return result;
 }
 
-// test here
-sumPrimes(10);
+chunkArrayInGroups(["a", "b", "c", "d"], 2);

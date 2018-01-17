abeb70db65c996572735b138b3a53c23d5ae0dfe
Kyle Mayers <kyle7150@gmail.com>
2018-01-17 22:20:03 +0000

    Daily FCC workout

--- FCC/todaysWorkOut.js
+++ FCC/todaysWorkOut.js
@@ -1,10 +1,15 @@
-function myReplace(str, before, after) {
-  var index = str.indexOf(before);
-  if (str[index] === str[index].toUpperCase()) {
-    after = after.charAt(0).toUpperCase() + after.slice(1);
-  }
-  str = str.replace(before, after);
+function translatePigLatin(str) {
+  var pigLatin = '';
+  var regex = /[aeiou]/gi;
+  if (str[0].match(regex)) {
+    pigLatin = str + 'way';
 
-  return str;
+  } else {
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + 'ay';
+  }
+
+  return pigLatin;
 }
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+translatePigLatin("consonant");

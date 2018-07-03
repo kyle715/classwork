6b02be3a17a06f5dfa0aa594d3972bd4be1c1bdf
Kyle Mayers <kyle7150@gmail.com>
2018-07-03 23:12:27 +0000

    DB was made

--- FCC/todaysWorkOut.js
+++ FCC/todaysWorkOut.js
@@ -1,2 +1,3 @@
 // TODAY WE LEARNED ABOUT GO-LANG
 //Working on rebuilding database for scraper project.
+// DB structure is set up in new skeleton web application to test if it is working correctly
